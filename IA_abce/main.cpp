72d6d6ac1aecba622055eef47be978ce930870ff
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2022-05-08 17:39:57 +0300

    operatii 1-3

--- IA_abce/main.cpp
+++ IA_abce/main.cpp
@@ -5,45 +5,128 @@ using namespace std;
 ifstream in("abce.in");
 ofstream out("abce.out");
 
-struct nod
+struct node
 {
     int valoare;
-    nod *leftChild;
-    nod *rightChild;
+    node *parent;
+    node *leftChild;
+    node *rightChild;
+    node(int);
 };
 
-nod *rad = nullptr;
+node::node(int val)
+{
+    this->valoare = val;
+    this->parent = nullptr;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+}
+
+node *rad = nullptr;
 
 void inserare(int val)
 {
-    nod *newNod = new nod;
-    nod *y = nullptr;
-    nod *x = rad;
+    node *newNode = new node(val);
+    node *y = nullptr;
+    node *x = rad;
 
     while(x != nullptr)
     {
         y = x;
-        if(val < x->valoare)
+        if(newNode->valoare < x->valoare)
             x = x->leftChild;
         else
             x = x->rightChild;
     }
 
-  //  newNod->parent = y;
+    newNode->parent = y;
     if(y == nullptr)
-        rad = newNod;
-    else if(newNod->valoare < y->valoare)
-        y->leftChild = newNod;
+        rad = newNode;
+    else if(newNode->valoare < y->valoare)
+        y->leftChild = newNode;
     else
-        y->rightChild = newNod;
+        y->rightChild = newNode;
 
 }
 
-void afis(nod *nodAfis)
+node* cautare(int val)    //cauta daca exista un node cu valoarea val
 {
-    while(rad != nullptr)
+    node *nodActual = rad;
+
+    while(nodActual != nullptr && val != nodActual->valoare)
+        if(val < nodActual->valoare)
+            nodActual = nodActual->leftChild;
+        else
+            nodActual = nodActual->rightChild;
+
+    return nodActual;
+}
+
+node* minimLocal(node *nodActual)     //cauta minimul de dupa nodul dat !NU E MINIMUL GLOBAL DIN ARBORE
+{
+    while(nodActual->leftChild != nullptr)
+        nodActual = nodActual->leftChild;
+
+    return nodActual;
+}
+
+node* succesor(int val)  //cauta si returneaza un pointer catre nodul care este succesor nodului cu valoarea val
+{
+    node *nodDeCautatSuccesor = cautare(val); //nodul caruia ii cauti succesor
+
+    if(nodDeCautatSuccesor->rightChild != nullptr)
+        return minimLocal(nodDeCautatSuccesor->rightChild);
+
+    node *y = nodDeCautatSuccesor->parent;
+
+    while(y != nullptr && nodDeCautatSuccesor == y->rightChild)
     {
-        cout<<nodAfis->valoare<<endl;
+        nodDeCautatSuccesor = y;
+        y = y->parent;
+    }
+
+    return y;
+
+}
+void sterge(int val)
+{
+    node *nodDeSters = cautare(val);
+
+    if(nodDeSters != nullptr)
+    {
+        node *y = nullptr;
+        node *x = nullptr;
+
+        if(nodDeSters->leftChild == nullptr || nodDeSters->rightChild == nullptr)
+            y = nodDeSters;
+        else
+            y = succesor(nodDeSters->valoare);
+
+        if(y->leftChild != nullptr)
+            x = y->leftChild;
+        else
+            x = y->rightChild;
+
+        if(x != nullptr)
+            x->parent = y->parent;
+
+        if(y->parent == nullptr)
+            rad = x;
+        else if(y == y->parent->leftChild)
+            y->parent->leftChild = x;
+        else
+            y->parent->rightChild = x;
+
+        if(y != nodDeSters)
+            nodDeSters->valoare = y->valoare;
+    }
+}
+
+void afis(node *nodAfis)
+{
+    if(nodAfis != nullptr)
+    {
+        cout<<nodAfis->valoare<<" "<<nodAfis<<endl;
         afis(nodAfis->leftChild);
         afis(nodAfis->rightChild);
     }
@@ -62,8 +145,25 @@ int main()
             in>>x;
             inserare(x);
         }
+        else if(op == 2)
+        {
+            in>>x;
+            sterge(x);
+        }
+        else if(op == 3)
+        {
+            in>>x;
+            if(cautare(x) != nullptr)
+                out<<1<<'\n';
+            else
+                out<<0<<'\n';
+        }
     }
 
-    //cout<<rad->valoare;
-  //  afis(rad);
+   // cout<<cautare(5)<<endl;
+    // cout<<rad->leftChild<<" "<<rad->rightChild;
+   // sterge(6);
+   //  afis(rad);
+
+   return 0;
 }
